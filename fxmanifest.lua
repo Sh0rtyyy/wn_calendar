5dd397d1b1ad6f65eb26adcdc3994914a1f4d8b3
Shorty <94754507+Sh0rtyyy@users.noreply.github.com>
2023-12-03 13:24:29 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,8 @@ game "gta5"
 
 lua54 "yes"
 
+description "A simle advent calendar script. Credits to Geerdo for date integration https://github.com/GeerdoDaGr8"
+
 version "2.0.0"
 
 client_scripts {
@@ -12,7 +14,7 @@ client_scripts {
 shared_script {
     "config.lua",
     '@ox_lib/init.lua',
-    '@es_extended/imports.lua' -- Uncomment if you are using ESX
+    '@es_extended/imports.lua' -- Comment if you are using ESX
 }
 
 server_scripts {
